846c98ec9522823ce8c48ed4a57cd3e42c116cc4
thesushi <joseph.franclin@gmail.com>
2016-03-20 22:10:42 +0100

    Adding default files

---
+++ themes/upjv/js/upjv.js
@@ -0,0 +1,35 @@
+   jQuery(document).ready(function ($) {
+            
+            var jssor_1_options = {
+              $ArrowNavigatorOptions: {
+                $Class: $JssorArrowNavigator$
+              },
+              $ThumbnailNavigatorOptions: {
+                $Class: $JssorThumbnailNavigator$,
+                $Cols: 15,
+                $SpacingX: 3,
+                $SpacingY: 3,
+                $Align: 455
+              }
+            };
+            
+            var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+            
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizing
+            function ScaleSlider() {
+                var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                if (refSize) {
+                    refSize = Math.min(refSize, 980);
+                    jssor_1_slider.$ScaleWidth(refSize);
+                }
+                else {
+                    window.setTimeout(ScaleSlider, 30);
+                }
+            }
+            ScaleSlider();
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            //responsive code end
+        });
